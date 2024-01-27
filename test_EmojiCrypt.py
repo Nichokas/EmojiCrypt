5f62015f71d96f268fe60a07ea544083d526c956
Nichokas <nicolas.rodalv@educa.jcyl.es>
2024-01-27 10:55:57 +0100

    cargando los test para probar la solucion

--- test_EmojiCrypt.py
+++ test_EmojiCrypt.py
@@ -1,4 +1,9 @@
 from EmojiCrypt import encrypt
 
 def test_letters():
-    assert encrypt(3, "Pez") == "🦿🤌🦁"
+    assert encrypt(3, "Pez") == "👂🤞🐆"
+    assert encrypt(256, "ElPezEstaRaro") == "❣️😟🥶❣️🧧❣️🕘🫨🖤🏓🖤🏓🌘"
+
+def test_caracteres():
+    assert encrypt(5, r" ,.;:-_/*-+()%&!?\"\$") == "💙🩶👱🙍🍍👳💃🍈🍍🍑🍅🥔🥒🥜🥯🍗🍞🍕"
+    assert encrypt(480, r" ,.;:-_/*-+()%&!?\"\$") == "☘️🕓🐔🚌👃🍾👹🪁🫣🍾📧🫛🎧💔🌓🏍️🐩🐦‍⬛🧱"
